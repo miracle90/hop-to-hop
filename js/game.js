aed9254c2a4e2cdada9597a5ef30bec9b574156b
李亚运(11112733) <11112733@vivo.xyz>
2020-11-24 21:18:20 +0800

    动画主流程

--- js/game.js
+++ js/game.js
@@ -9,6 +9,7 @@ class Game {
 			cubeHeight: 2,
 			cubeDeep: 4,
 			jumperColor: 0x232323,
+			jumperWidth: 1,
 			jumperHeight: 2,
 			jumperDeep: 1
 		}
@@ -32,14 +33,14 @@ class Game {
 		this.cubeStat = {
 			nextDir: ''
 		}
-		this.failedStat = {
+		this.falledStat = {
 			// 默认落在当前块上
 			location: -1,
 			// 距离，根据距离判断是否倒下去
 			distance: 0
 		}
 		// 是否下落完成
-		this.tallingStat = {
+		this.fallingStat = {
 			end: false,
 			speed: 0.2
 		}
@@ -52,14 +53,31 @@ class Game {
 	}
 
 	init() {
+		this._addAxisHelp()
 		this._setCamera()
 		this._setRenderer()
 		this._setLight()
 		this._createCube()
+		this._createCube()
+		this._createJumper()
+		this._updateCamera()
 		this._handleWindowResize()
 		window.addEventListener('resize', () => {
 			this._handleWindowResize()
 		})
+		// 给当前canvas绑定事件
+		let canvas = document.querySelector('canvas')
+		canvas.addEventListener('mousedown', () => {
+			this._handleMouseDown()
+		})
+		canvas.addEventListener('mouseup', () => {
+			this._handleMouseUp()
+		})
+	}
+
+	_addAxisHelp() {
+		let axis = new THREE.AxisHelper(20)
+		this.scene.add(axis)
 	}
 
 	_handleWindowResize() {
@@ -71,6 +89,153 @@ class Game {
 		this.camera.updateProjectionMatrix()
 		this.renderer.setSize(this.size.width, this.size.height)
 		this._render()
+	}
+
+	_handleMouseDown() {
+		if (!this.jumperStat.ready) {
+			// y方向，压缩当前块
+			this.jumper.scale.y -= 0.01
+			this.jumperStat.xSpeed += 0.004
+			this.jumperStat.ySpeed += 0.008
+			this._render()
+			requestAnimationFrame(() => {
+				this._handleMouseDown()
+			})
+		}
+	}
+
+	_handleMouseUp() {
+		this.jumperStat.ready = true
+		// >= 1还在空中，没有结束跳跃过程
+		if (this.jumper.position.y >= 1) {
+			if (this.jumper.scale.y < 1) {
+				this.jumper.scale.y += 0.1
+			}
+			// 如果往左边跑
+			if (this.cubeStat.nextDir === 'left') {
+				this.jumper.position.x -= this.jumperStat.xSpeed
+			} else {
+				this.jumper.position.z -= this.jumperStat.xSpeed
+			}
+			this.jumperStat.ySpeed -= 0.01
+			this.jumper.position.y += this.jumperStat.ySpeed
+			this._render()
+			requestAnimationFrame(() => {
+				this._handleMouseUp()
+			})
+		} else {
+			this.jumperStat.ready = false
+			// 恢复默认值
+			this.jumper.scale.y = 1
+			this.jumper.position.y = 1
+			this.jumper.xSpeed = 0
+			this.jumper.ySpeed = 0
+			// 检测落在哪里了
+			this._checkInCube()
+			if (this.falledStat.location === 1) {
+				// 如果成功了
+				this.score++
+			} else {
+				// 失败
+				this._falling()
+			}
+		}
+	}
+
+	_checkInCube() {
+		// -1：当前盒子
+		// -10：当前盒子上滑下去
+		// 1：跳到下一个盒子
+		// 10：从下一个盒子上掉落
+		// 0：没有落在盒子上
+		let distanceCur, distanceNext
+		let should = (this.config.cubeWidth + this.config.jumperWidth) / 2
+		if (this.cubeStat.nextDir === 'left') {
+			distanceCur = Math.abs(this.cubes[this.cubes.length - 2].position.x - this.jumper.position.x)
+			distanceNext = Math.abs(this.cubes[this.cubes.length - 1].position.x - this.jumper.position.x)
+		} else {
+			distanceCur = Math.abs(this.cubes[this.cubes.length - 2].position.z - this.jumper.position.z)
+			distanceNext = Math.abs(this.cubes[this.cubes.length - 1].position.z - this.jumper.position.z)
+		}
+
+		if (distanceCur < should) {
+			// 落在当前块
+			this.falledStat.location = distanceCur < this.config.cubeWidth / 2 ? -1 : -10
+		} else if (distanceNext < should) {
+			// 落在下一个块
+			this.falledStat.location = distanceNext < this.config.cubeWidth / 2 ? 1 : 10
+		} else {
+			// 没有落在块上
+			this.falledStat.location = 0
+		}
+	}
+
+	_falling() {
+		// -10，10，0
+		// -10从当前盒子落下，左前或者右前方 leftTop rightTop
+		// 10从下一个盒子落下 leftTop leftBottom rightTop rightBottom
+		// 0 none
+		if (this.falledStat.location === 10) {
+			if (this.cubeStat.nextDir === 'left') {
+				if (this.jumper.position.x > this.cubes[this.cubes.length - 1].position.x) {
+					this._fallingDir('leftBottom')
+				} else {
+					this._fallingDir('leftTop')
+				}
+			} else {
+				if (this.jumper.position.z > this.cubes[this.cubes.length - 1].position.z) {
+					this._fallingDir('rightBottom')
+				} else {
+					this._fallingDir('rightTop')
+				}
+			}
+		} else if (this.falledStat.location === -10) {
+			if (this.cubeStat.nextDir === 'left') {
+				this._fallingDir('leftTop')
+			} else {
+				this._fallingDir('rightTop')
+			}
+		} else if (this.falledStat.location === 0) {
+			this._fallingDir('none')
+		}
+	}
+
+	_fallingDir(dir) {
+		console.log(dir)
+		let offset = this.falledStat.distance - this.config.cubeWidth / 2
+		let isRotate = true
+		let axis = dir.includes('left') ? 'z' : 'x'
+		let rotate = this.jumper.rotation[axis]
+		let fallingTo = this.config.ground + this.config.jumperWidth / 2
+		if (dir === 'leftTop') {
+			rotate += 0.1
+			isRotate = this.jumper.rotation[axis] < Math.PI / 2
+		} else if (dir === 'leftBottom') {
+			rotate -= 0.1
+			isRotate = this.jumper.rotation[axis] < -Math.PI / 2
+		} else if (dir === 'rightTop') {
+			rotate -= 0.1
+			isRotate = this.jumper.rotation[axis] < -Math.PI / 2
+		} else if (dir === 'rightBottom') {
+			rotate += 0.1
+			isRotate = this.jumper.rotation[axis] < Math.PI / 2
+		} else if (dir === 'none') {
+			fallingTo = this.config.ground
+			isRotate = false
+		}
+		if (!this.fallingStat.end) {
+			if (isRotate) {
+				this.jumper.rotation[axis] = rotate
+			} else if (this.jumper.position.y > fallingTo) {
+				this.jumper.position.y -= this.fallingStat.speed
+			} else {
+				this.fallingStat.end = true
+			}
+			this._render()
+			requestAnimationFrame(() => {
+				this._falling()
+			})
+		}
 	}
 
 	// 设置相机位置
@@ -104,7 +269,57 @@ class Game {
 			}
 		}
 		this.cubes.push(cube)
+		if (this.cubes.length > 6) {
+			// 删除第一项，并且从场景中删除
+			this.scene.remove(this.cubes.shift())
+		}
 		this.scene.add(cube)
+		if (this.cubes.length > 1) {
+			// 更新镜头的位置
+			this._updateCameraPos()
+		}
+	}
+	_createJumper() {
+		let geometry = new THREE.CubeGeometry(this.config.jumperWidth, this.config.jumperHeight, this.config.jumperDeep)
+		let material = new THREE.MeshLambertMaterial({ color: this.config.jumperColor })
+		this.jumper = new THREE.Mesh(geometry, material)
+		// 游戏结束，底部开始旋转，抬高1
+		geometry.translate(0, 1, 0)
+		this.jumper.position.y = 1
+		this.scene.add(this.jumper)
+	}
+
+	// 更新镜头的位置
+	_updateCameraPos() {
+		// 计算出next，镜头对准最后两个块的最中间，数组的倒数第二个和最后一个
+		let lastIndex = this.cubes.length - 1
+		let pointA = {
+			x: this.cubes[lastIndex - 1].position.x,
+			z: this.cubes[lastIndex - 1].position.z
+		}
+		let pointB = {
+			x: this.cubes[lastIndex].position.x,
+			z: this.cubes[lastIndex].position.z
+		}
+
+		this.cameraPos.next = new THREE.Vector3((pointA.x + pointB.x) / 2, 0, (pointA.z + pointB.z) / 2)
+	}
+
+	// 改变相机的镜头
+	_updateCamera() {
+		if (this.cameraPos.curent.x > this.cameraPos.next.x || this.cameraPos.curent.z > this.cameraPos.next.z) {
+			if (this.cubeStat.nextDir === 'left') {
+				this.cameraPos.curent.x -= 0.1
+			} else {
+				this.cameraPos.curent.z -= 0.1
+			}
+
+			this.camera.lookAt(this.cameraPos.curent)
+			this._render()
+			requestAnimationFrame(() => {
+				this._updateCamera()
+			})
+		}
 	}
 
 	// 设置灯光
